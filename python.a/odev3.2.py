87bd2f9daae937b916839ec4ab37c87951cb4128
Ceyda Ankara <162893709+ceydankara@users.noreply.github.com>
2025-02-19 15:09:10 +0300

    Create odev3.2.py

---
+++ python.a/odev3.2.py
@@ -0,0 +1,8 @@
+import odev3 as a #bu dosyada odev3 dosyasını çagırmak için a ile cagıracagız artık
+import random #random sayı üretmek için pythonun kendi dosyası
+
+print(a.topla(10,5)) #dosyalar arası iletisim için
+
+print(random.randint(0,100))
+
+
