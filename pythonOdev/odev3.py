92a2c935f31ce3eeb86da6c7125f9bc37765d6fe
Ceyda Ankara <162893709+ceydankara@users.noreply.github.com>
2025-03-08 00:45:28 +0300

    odev3.py

--- pythonOdev/odev3.py
+++ pythonOdev/odev3.py
@@ -15,4 +15,17 @@ a=1
 for i in range(1,sayi+1):
     a=a*i
 print(a)
- 
+
+
+ for i in range(2, 100):  
+    
+    for j in range(2, int(i ** 0.5) + 1): 
+        if i % j == 0:
+            asal_mi = False  
+            break  
+    
+    if asal_mi:
+        print(i, "asal sayıdır.")
+     
+
+
