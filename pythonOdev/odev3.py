71710a7205da15d93bc91a08ee237628153a748c
Ceyda Ankara <162893709+ceydankara@users.noreply.github.com>
2025-03-07 23:59:35 +0300

    Create odev3.py

---
+++ pythonOdev/odev3.py
@@ -0,0 +1,18 @@
+
+for i in range(1,100):
+ print(i)
+
+print("*****************")
+
+for i in range(0,100,2):
+ print(i)
+
+print("*****************")
+
+sayi=int(input("Sayi giriniz: "))
+
+a=1
+for i in range(1,sayi+1):
+    a=a*i
+print(a)
+ 
